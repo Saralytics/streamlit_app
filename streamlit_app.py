8f01639f6f8e552bedb538020f4c9e20efac6a4d
Saralytics <61179765+Saralytics@users.noreply.github.com>
2023-01-24 09:04:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit 
+import pandas 
 
 streamlit.title("My parent's healthy diner")
-streamlit.header("Breakfast menu")
+streamlit.header("Breakfast menu")                  
+streamlit.header("Build your own fruit smoothie")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
