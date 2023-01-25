19afdae9bd5bf055516dd49ce52be7ca89e86734
Saralytics <61179765+Saralytics@users.noreply.github.com>
2023-01-25 17:11:08 +0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,9 +25,14 @@ streamlit.dataframe(fruityvice_normalized)
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
-my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_cur.execute("SELECT * from fruit_load_list")
 my_data_row = my_cur.fetchone()
 streamlit.text("Hello from Snowflake:")
 streamlit.text(my_data_row)
 
 add_fruit = streamlit.text_input("What fruit do you want to add?","Write fruit name here")
+
+my_cur.execute("insert into fruit_load_list values ({})".format(add_fruit))
+
+streamlit.text("Thank you for adding {}".format(add_fruit))
+
